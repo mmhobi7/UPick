a84ba83eb3d6885e1834cacb51c4140bcb2f62b1
Shane Malone <shanemalone@ufl.edu>
2021-04-16 14:49:47 -0400

    updated main

--- TestProj3a.cpp
+++ TestProj3a.cpp
@@ -1,26 +1,36 @@
-// TestProj3a.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <unordered_map>
+#include <queue>
+using namespace std;
+
+struct App {
+    unordered_map<string,Restaurant> allRestaurants; // make this the unordered_set<std::pair<string,Restaurant>>
+    vector<Category> categoryList;
+};
+class Category {
+    unordered_map<string, Restaurant> restaurantList; // make this an unordered_set
+    vector<string> listNames;
+    priority_queue<Restaurant> maxHeap; // based on rating. comparator uses restaurant.rating
+};
+class Restaurant {
+    string ID;
+    string name;
+    double rating;
+    string address;
+    string category;
+public:
+};
 
 int main()
 {
+    App uPick;
     std::cout << "Welcome to YouPick!\n\n";
     std::cout << "Choose a Preferred Cuisine Style [if any]:\n";
+    for (int i = 1; i <= uPick.categoryList.size(); ++i) {
+        std::cout << i << ": " <<  endl;
+    }
     std::cout << "1. Italian\n2. Chinese\n3. Indian\n4. American\n5. Mexican\n6. All\n\n";
     std::cout << "Great Choice! Picking a restaurant now...\n\n";
     std::cout << "We picked \"Aman's Place\"! Would you like to see related restaurants? (Y/N)\n\n";
     std::cout << "Okay! Enjoy your meal!\n";
-
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
