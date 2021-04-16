7a1118f9657676b3187d85a4c075cb216b5db7d5
Shane Malone <shanemalone@ufl.edu>
2021-04-16 14:54:03 -0400

    int* heapifyDown(int arr[], int size, int index) {     int childLeft = 2*index + 1;     int childRight = 2*index + 2;     if (childLeft > size - 1)         return arr;     int childMax = 0;     if (arr[childLeft] > arr[childRight])         childMax = childLeft;     else         childMax = childRight;     if (arr[childMax] > arr[index]) {         int tempVal = arr[index];         int nextIndex = childMax;         arr[index] = arr[childMax];         arr[childMax] = tempVal;         return heapifyDown(arr,size,childMax);     }     else         return arr;
    }
    int* extractMax(int arr[], int size)
    {
        arr[0] = arr[--size];
        return heapifyDown(arr, size, 0);
    
    }
    
    //Don't change main
    int main() {
    
       int size;
       cin >> size;
       int* arr = new int[size];
    
        for(int i = 0; i < size; i++)
            cin >> arr[i];
    
       arr =  extractMax(arr, size);
       for(int i = 0; i < size - 1; i++)
          cout << arr[i] << " ";
    
       return 0;
    }

--- maxHeap_malone.cpp
+++ maxHeap_malone.cpp
@@ -42,4 +42,95 @@ void heapRemove(int* theheap, int& size) {
 void heapPrint(int* theheap, int size) {
     for (int i = 0; i < size; ++i)
         std::cout << theheap[i] << " ";
+}int* heapifyDown(int arr[], int size, int index)
+{
+    int childLeft = 2*index + 1;
+    int childRight = 2*index + 2;
+    if (childLeft > size - 1)
+        return arr;
+    int childMax = 0;
+    if (arr[childLeft] > arr[childRight])
+        childMax = childLeft;
+    else
+        childMax = childRight;
+    if (arr[childMax] > arr[index]) {
+        int tempVal = arr[index];
+        int nextIndex = childMax;
+        arr[index] = arr[childMax];
+        arr[childMax] = tempVal;
+        return heapifyDown(arr,size,childMax);
+    }
+    else
+        return arr;
+    
+}
+int* extractMax(int arr[], int size)
+{
+    arr[0] = arr[--size];
+    return heapifyDown(arr, size, 0);
+    
+}
+
+
+//Don't change main
+int main() {
+
+   int size;
+   cin >> size;
+   int* arr = new int[size];
+
+    for(int i = 0; i < size; i++)
+        cin >> arr[i];
+
+   arr =  extractMax(arr, size);
+   for(int i = 0; i < size - 1; i++)
+      cout << arr[i] << " ";
+
+   return 0;
+}
+int* heapifyDown(int arr[], int size, int index)
+{
+    int childLeft = 2 * index + 1;
+    int childRight = 2 * index + 2;
+    if (childLeft > size - 1)
+        return arr;
+    int childMax = 0;
+    if (arr[childLeft] > arr[childRight])
+        childMax = childLeft;
+    else
+        childMax = childRight;
+    if (arr[childMax] > arr[index]) {
+        int tempVal = arr[index];
+        int nextIndex = childMax;
+        arr[index] = arr[childMax];
+        arr[childMax] = tempVal;
+        return heapifyDown(arr, size, childMax);
+    }
+    else
+        return arr;
+
+}
+int* extractMax(int arr[], int size)
+{
+    arr[0] = arr[--size];
+    return heapifyDown(arr, size, 0);
+
+}
+
+
+//Don't change main
+int main() {
+
+    int size;
+    cin >> size;
+    int* arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        cin >> arr[i];
+
+    arr = extractMax(arr, size);
+    for (int i = 0; i < size - 1; i++)
+        cout << arr[i] << " ";
+
+    return 0;
 }
