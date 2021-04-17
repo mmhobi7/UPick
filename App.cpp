cb02bdb3e124df1bbca4a9f6b30edc23879816a8
Muhamed Hobi <woohoomoo2u@gmail.com>
2021-04-17 16:41:55 -0400

    Merge part 900

--- App.cpp
+++ App.cpp
@@ -14,18 +14,52 @@ void App::SetCategoryList() {
     categoryList.push_back("Indian");
     categoryList.push_back("Latin");
     categoryList.push_back("Mediterranean");
+    categoryList.push_back("Mexican");
     categoryList.push_back("Pizza");
+    categoryList.push_back("Seafood");
     categoryList.push_back("Vegan");
+}
+
+void App::SetCategorySet() {
+    categorySet.insert("American");
+    categorySet.insert("Asian");
+    categorySet.insert("Bars");
+    categorySet.insert("Breakfast");
+    categorySet.insert("Fast");
+    categorySet.insert("Halal");
+    categorySet.insert("Indian");
+    categorySet.insert("Latin");
+    categorySet.insert("Mediterranean");
+    categorySet.insert("Mexican");
+    categorySet.insert("Pizza");
+    categorySet.insert("Seafood");
+    categorySet.insert("Vegan");
 }
 
 vector<string> App::GetCategoryList() {
     return categoryList;
 }
 
-void App::AddRestaurant(Restaurant* obj) {
-    allRestaurants[obj->GetCategory()].push_back(obj);
+string App::FindCategory(string s) {
+    if (categorySet.find(s) != categorySet.end()) {
+        return s;
+    }
+    else if (s == "Chinese" || s == "Japanese" || s == "Korean") {
+        return "Asian";
+    }
+    else if (s == "Greek") {
+        return "Mediterranean";
+    }
+    else if (s == "Mexican" || s == "Cuban" || s == "Tex-Mex" || s == "Peruvian" || s == "Colombian") {
+        return "Latin";
+    }
+    else if (s == "Vegetarian") {
+        return "Vegan";
+    }
+    else
+        return "";
 }
 
-int App::GetRestaurantListSize() {
-    return allRestaurants.size();
+void App::AddRestaurant(Restaurant* obj) {
+    allRestaurants[obj->GetCategory()].push_back(obj);
 }
