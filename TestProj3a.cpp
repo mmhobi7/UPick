f4daa64e4a529f89731b183a15141bbacc74078e
Nathan Yant <wyly.yant@gmail.com>
2021-04-18 16:43:31 -0400

    TestProj3a.cpp

--- TestProj3a.cpp
+++ TestProj3a.cpp
@@ -3,46 +3,88 @@
 
 int main()
 {
+    srand(time(NULL)); // set random's seed
     App uPick;
     // read in all data
-    std::cout << "Welcome to YouPick!\n\n";
-    std::cout << "Choose a Preferred Cuisine Style [if any]:\n";
-    for (int i = 1; i <= uPick.getCategoryList().size(); ++i) {
-        std::cout << i << ": " << uPick.getCategoryList()[i-1] << endl;
+    std::cout << "Welcome to YouPick! \n\n";
+    std::cout << "Choose a Preferred Cuisine Style [if any]: \n";
+    int i;
+    for (i = 1; i <= uPick.getCategoryList().size(); ++i)
+    {
+        std::cout << i << ": " << uPick.getCategoryList()[i - 1] << endl;
     }
+    std::cout << i++ << ": "
+              << "All" << endl;
     std::cout << endl;
     int option = 0;
     cin >> option;
-    Restaurant* chosen;
-    dataImporter importer("yelp_business.csv");
-    Graph tmp;
-    importer.read(uPick, tmp);
-    // if all, generate random cuisine
-        if(option == 14) {
-            option = rand() % 13;
-        }
-
-        int size = uPick.getCategorySize(option);
-        int randZipCode = rand() % size;
-
-        size = uPick.getZipcodeSize(option, randZipCode);
-        int randRest = rand() % size;
-        chosen = uPick.getRestaurant(option, randZipCode, randRest);
-    // generate heap and graph from this restaurant
-    
-    
-    std::cout << "\nGreat Choice! Picking a restaurant now...\n\n";
-    std::cout << "We picked \" << chosen->getName() << "\"! Would you like to see related restaurants? (Y/N)\n\n";
-    bool moreRestaurants = true;
     char choice;
-    queue<Restaurant*> related = myGraph.bfs(chosen);
-    
-    while (moreRestaurants) {
-            cin >> choice;
-            if (choice == 'Y') {
-                std::cout << "Would you like to see more?" << endl;
-                //print out 5 more choices while maxHeap is not empty
-                if(related.size() > 5) {
+    Restaurant *chosen;
+    dataImporter importer("/Users/wylyyant/CLionProjects/YouPickProject/yelp_business.csv");
+    Graph tmp4;
+    importer.read(uPick, tmp4);
+    // if all, generate random cuisine
+    std::cout << "Do you wish to enter a zipcode? (Y/N)" << endl;
+    cin >> choice;
+    int zipcode = 0;
+    int size = 0;
+    int thesize = 0;
+    auto tmp3 = uPick.getList().begin();
+    /*cout << "here: " << tmp3->first << endl;
+    advance(tmp3,2);
+    cout << "here: " << tmp3->first << endl; */
+
+    /*for (auto tmp2 = uPick.getList().begin(); tmp2 != uPick.getList().end(); tmp2++)
+    {
+        cout << "here: " << tmp2->first << endl;
+        // for (auto j = (uPick.getList()[i->second]).begin(); j != (uPick.getList()[i->second]).end(); j++)
+        // {
+        //     cout << j->first << " | " << j->second.size() << " | " << thesize++ << endl;
+        // }
+    } */
+
+    if (option == 15)
+    {
+        option = rand() % 15;
+    }
+    if (choice == 'Y' || choice == 'y')
+    {
+        cin >> zipcode;
+        zipcode = uPick.findZip(zipcode, uPick.getCategoryList()[option - 1]);
+    }
+    else
+    {
+        size = uPick.getCategorySize(option);
+        zipcode = rand() % size;
+        cout << "random number we generated: " << zipcode << " " << size << endl;
+        //auto tmp = uPick.getList()[uPick.getCategoryList()[option - 1]].begin();
+        auto tmp = uPick.getList();
+        auto categoryList = uPick.getCategoryList();
+        auto curr = tmp[categoryList[option - 1]].begin();
+        advance(curr, zipcode);
+        zipcode = curr->first;
+        cout << "Random zipcode is " << zipcode << endl; //TODO: pad with 0's
+    }
+    size = uPick.getZipcodeSize(option, zipcode);
+    int randRest = rand() % size;
+    chosen = uPick.getRestaurant(option, zipcode, randRest);
+
+    // generate heap and graph from this restaurant
+
+    std::cout << "\nGreat Choice! Picking a restaurant now...\n\n";
+    std::cout << "We picked " << chosen->getName() << endl
+              << "Would you like to see related restaurants? (Y/N)\n\n";
+    bool moreRestaurants = true;
+
+    queue<Restaurant*> related = tmp4.bfs(chosen);
+
+    while (moreRestaurants)
+    {
+        cin >> choice;
+        if (choice == 'Y' || choice == 'y')
+        {
+            //print out 5 more choices while maxHeap is not empty
+            if(related.size() > 5) {
                     size = 5;
                 } else {
                     size = related.size();
@@ -51,14 +93,14 @@ int main()
                     related.front()->print();
                     related.pop();
                 }
-            }
-            else
-                moreRestaurants = false;
+            std::cout << "Would you like to see more?" << endl;
         }
+        else
+            moreRestaurants = false;
+    }
     std::cout << "Okay! Enjoy your meal!\n";
     return 0;
 }
-
 
 
 
