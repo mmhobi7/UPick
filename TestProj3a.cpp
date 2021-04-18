88820713833127a2a464891c95980b44ff4b809d
Nathan Yant <wyly.yant@gmail.com>
2021-04-17 21:37:08 -0400

    TestProj3a.cpp

--- TestProj3a.cpp
+++ TestProj3a.cpp
@@ -47,7 +47,7 @@ int main()
                 } else {
                     size = related.size();
                 }
-                for(int i = 0; i < 5; i++) {
+                for(int i = 0; i < size; i++) {
                     related.front()->print();
                     related.pop();
                 }
